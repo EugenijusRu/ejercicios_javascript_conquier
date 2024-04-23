38c24a68e064746b55339f03d1e8b0573506fc74
Eugenio <eugenijus.gendvilas@yahoo.com>
2024-04-23 20:43:01 +0200

    Segundo ejercicio

---
+++ heladeria.js
@@ -0,0 +1,9 @@
+//Crear un programa que pida al usuario ingresar, por separado, tres gustos de helado,
+// y luego muestre un mensaje que diga, por ejemplo, 
+// "Aquí tiene su helado de chocolate, dulce de leche y menta granizada"
+
+let helado1 = prompt("Por favor ingresar el primer gusto de helado");
+let helado2 = prompt("Por favor ingresar el segundo gusto de helado");
+let helado3 = prompt("Por favor ingresar el trecer gusto de helado");
+
+console.log(`Àqui tiene su helado de ${helado1}, ${helado2} y ${helado3}`);
