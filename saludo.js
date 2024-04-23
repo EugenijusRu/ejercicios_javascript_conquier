6fd2d2f4931571a10c4138d68eb3a9171823ebc5
Eugenio <eugenijus.gendvilas@yahoo.com>
2024-04-23 20:17:15 +0200

    Mi primer ejercicio

---
+++ saludo.js
@@ -0,0 +1,6 @@
+//Crear un programa que pida al usuario ingresar un nombre y apellido por separado,
+//y luego muestre un mensaje que diga, "Hola {nombre} {apellido}, bienvenida a Ada".
+
+let nombre = prompt("Por favor ingresar tu nombre");
+let apellido = prompt("Por favor ingresar tu apellido");
+console.log(`Hola ${nombre} ${apellido}, bienvenido a Ada`);
